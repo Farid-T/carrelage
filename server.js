0663891d7d70ad88d143182bde6ddb09d4e14111
Farid-T <startmob@gmail.com>
2022-12-12 13:08:13 +0100

    rectif exercice carrelage

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-// const https = require('https')
+const https = require('https')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -13,11 +13,11 @@ app.post("/", function (req, res) {
   const surf = req.body.surface;
   const nbreDeCarreaux = (45) * surf;
   
-  res.send("Pour votre surface de" + surf + "Le nombre de carreaux nécéssaire est de" + nbreDeCarreaux + "m2")
+  res.send("Pour votre surface de " + surf + " m2. Le nombre de carreaux nécéssaire est de : " + nbreDeCarreaux + " unités")
 })
 
 
 
-app.listen(3005, function() {
-  console.log('Exemple app listening on port 3000!')
+app.listen(3004, function() {
+  console.log('connexion établie')
 })
