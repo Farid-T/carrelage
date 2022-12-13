5321d73e37488798c33031dfa00a58338e0141a2
Farid-T <startmob@gmail.com>
2022-12-13 09:26:05 +0100

    modif

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/calculCarrelage.html")
 })
-
-app.post("/", function (req, res) {
+app.post("/", function (req, res) { 
   const surf = req.body.surface;
   const nbreDeCarreaux = (45) * surf;
-  
-  res.send("Pour votre surface de " + surf + " m2. Le nombre de carreaux nécéssaire est de : " + nbreDeCarreaux + " unités")
+  res.send("Pour votre surface de " + surf + "m2. Le nombre de carreaux nécéssaire est de : " + nbreDeCarreaux + " unités")
+})
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/calculCarrelage.html")
+}) 
+
+app.post("/", function (req, res) {
+  const surf1 = req.body.prix;
+  const prix = surf1 * (49.90)
+res.send("Pour votre surface de " + surf1 + "m2. Le prix total de votre commande est de : " + prix + "€")
 })
 
 
